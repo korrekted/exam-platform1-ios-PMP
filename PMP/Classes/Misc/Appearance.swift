20afe69a6dd8835e10fa22ea13978ec493147844
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-28 21:11:00 +0300

    copy

---
+++ PMP/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 255, green: 250, blue: 249)
+    
+    static let mainColor = UIColor(integralRed: 16, green: 87, blue: 102)
+    static let mainColorAlpha = UIColor(integralRed: 16, green: 87, blue: 102, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 254, green: 132, blue: 84)
+    
+    static let progress1Color = UIColor(integralRed: 16, green: 87, blue: 102)
+    static let progress2Color = UIColor(integralRed: 254, green: 132, blue: 84)
+    static let progress3Color = UIColor(integralRed: 21, green: 170, blue: 154)
+}
---
+++ PMP/Classes/Misc/GlobalDefinitions.swift
@@ -0,0 +1,25 @@
+//
+//  GlobalDefinitions.swift
+//  Nursing
+//
+//  Created by Andrey Chernyshev on 16.01.2021.
+//
+
+final class GlobalDefinitions {
+    static let domainUrl = "https://exams.korrekted.com"
+    static let apiKey = "VNqFFs8tGOWGcrxm"
+
+    static let sdkDomainUrl = "https://analytics.korrekted.com"
+    static let sdkApiKey = "7MZjF0tuPEby948z"
+
+    static let applicationTag = "EXAM_PMP"
+
+    static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
+    static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+
+    static let appleAppID = "1569346507"
+
+    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
+    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
+    static let contactUsUrl = "https://quantresear.ch/contact.html"
+}
