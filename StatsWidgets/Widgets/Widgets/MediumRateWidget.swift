6b93464a639b9cedd5db2f7324c417c1ac52e6fa
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-08-20 09:52:53 +0300

    new version; build 3

---
+++ StatsWidgets/Widgets/Widgets/MediumRateWidget.swift
@@ -0,0 +1,52 @@
+//
+//  SmallRateWidget.swift
+//  StatsWidgetsExtension
+//
+//  Created by Andrey Chernyshev on 12.07.2021.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct MediumRateWidget: Widget {
+    let placeholder = StatsContent(date: Date(),
+                                   passRate: 75,
+                                   testTaken: 0,
+                                   testsTakenNum: 10,
+                                   correctAnswers: 0,
+                                   correctAnswersNum: 374,
+                                   questionsTaken: 0,
+                                   answeredQuestions: 400)
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: "MediumRateWidget", provider: Provider(placeholder: placeholder)) { entry in
+            VStack(alignment: .leading) {
+                VStack(alignment: .center) {
+                    PassRateView(title: "Widgets.PassRate".localized,
+                                 progressFontSize: 19.scale,
+                                 progress: entry.passRate / 100,
+                                 titleColor: Color.make(254, green: 132, blue: 84),
+                                 progressColor: Color.make(16, green: 87, blue: 102))
+                    Spacer()
+                    HStack() {
+                        MediumVerticalRateView(count: entry.testsTakenNum,
+                                               title: "Widgets.TestsTaken".localized,
+                                               color: Color.make(254, green: 132, blue: 84))
+                        Spacer()
+                        MediumVerticalRateView(count: entry.correctAnswersNum,
+                                               title: "Widgets.CorrectAnswers".localized,
+                                               color: Color.make(16, green: 87, blue: 102))
+                        Spacer()
+                        MediumVerticalRateView(count: entry.answeredQuestions,
+                                               title: "Widgets.QuestionsTaken".localized,
+                                               color: Color.make(21, green: 170, blue: 154))
+                    }
+                }
+                .padding(16)
+            }
+        }
+        .supportedFamilies([.systemMedium])
+        .configurationDisplayName("Widgets.Medium.Name".localized)
+        .description("Widgets.Medium.Description".localized)
+    }
+}
