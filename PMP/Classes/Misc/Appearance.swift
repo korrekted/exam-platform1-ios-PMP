2d6336dc55a0d1ebdc3d6dfd7107f9d23b2c627c
akonst17 <akonst17@gmail.com>
2022-08-01 20:41:43 +0300

    change appStoreUrl; build 11

--- PMP/Classes/Misc/Appearance.swift
+++ PMP/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 255, green: 250, blue: 249)
-    
     static let mainColor = UIColor(integralRed: 16, green: 87, blue: 102)
     static let mainColorAlpha = UIColor(integralRed: 16, green: 87, blue: 102, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 254, green: 132, blue: 84)
-    
-    static let progress1Color = UIColor(integralRed: 16, green: 87, blue: 102)
-    static let progress2Color = UIColor(integralRed: 254, green: 132, blue: 84)
-    static let progress3Color = UIColor(integralRed: 21, green: 170, blue: 154)
-    
+    static let secondColor = UIColor(integralRed: 21, green: 170, blue: 154)
+    static let thirdColor = UIColor(integralRed: 254, green: 132, blue: 84)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 255, green: 249, blue: 247)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
